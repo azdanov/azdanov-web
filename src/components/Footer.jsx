2379dd7cb73a0a525f12c8f09e547a8242cf3a42
Anton Ždanov <anton@azdanov.dev>
2023-03-06 04:59:57 +0200

    fix lint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,6 @@ export function Footer() {
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
-
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Anton Ždanov. All rights
