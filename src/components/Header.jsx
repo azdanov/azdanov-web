924134e8a766e554498624191dfacee75fb38da4
Anton Ždanov <anton@azdanov.dev>
2023-03-17 06:50:08 +0200

    extract functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -191,6 +191,82 @@ function Avatar({ large = false, className, ...props }) {
   );
 }
 
+function setProperty(property, value) {
+  document.documentElement.style.setProperty(property, value);
+}
+
+function removeProperty(property) {
+  document.documentElement.style.removeProperty(property);
+}
+
+function updateHeaderStyles({ headerRef, isInitial, downDelay, upDelay }) {
+  const { top, height } = headerRef.current.getBoundingClientRect();
+  const scrollY = clamp(
+    window.scrollY,
+    0,
+    document.body.scrollHeight - window.innerHeight
+  );
+
+  if (isInitial.current) {
+    setProperty("--header-position", "sticky");
+  }
+
+  setProperty("--content-offset", `${downDelay}px`);
+
+  if (isInitial.current || scrollY < downDelay) {
+    setProperty("--header-height", `${downDelay + height}px`);
+    setProperty("--header-mb", `${-downDelay}px`);
+  } else if (top + height < -upDelay) {
+    const offset = Math.max(height, scrollY - upDelay);
+    setProperty("--header-height", `${offset}px`);
+    setProperty("--header-mb", `${height - offset}px`);
+  } else if (top === 0) {
+    setProperty("--header-height", `${scrollY + height}px`);
+    setProperty("--header-mb", `${-scrollY}px`);
+  }
+
+  if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
+    setProperty("--header-inner-position", "fixed");
+    removeProperty("--header-top");
+    removeProperty("--avatar-top");
+  } else {
+    removeProperty("--header-inner-position");
+    setProperty("--header-top", "0px");
+    setProperty("--avatar-top", "0px");
+  }
+}
+
+function updateAvatarStyles({ isHomePage, downDelay }) {
+  if (!isHomePage) {
+    return;
+  }
+
+  const fromScale = 1;
+  const toScale = 36 / 64;
+  const fromX = 0;
+  const toX = 2 / 16;
+
+  const scrollY = downDelay - window.scrollY;
+
+  let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale;
+  scale = clamp(scale, fromScale, toScale);
+
+  let x = (scrollY * (fromX - toX)) / downDelay + toX;
+  x = clamp(x, fromX, toX);
+
+  setProperty(
+    "--avatar-image-transform",
+    `translate3d(${x}rem, 0, 0) scale(${scale})`
+  );
+
+  const borderScale = 1 / (toScale / scale);
+  const borderX = (-toX + x) * borderScale;
+  const borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`;
+
+  setProperty("--avatar-border-transform", borderTransform);
+  setProperty("--avatar-border-opacity", scale === toScale ? 1 : 0);
+}
+
 export function Header() {
   const isHomePage = useRouter().pathname === "/";
 
@@ -202,85 +278,9 @@ export function Header() {
     const downDelay = avatarRef.current?.offsetTop ?? 0;
     const upDelay = 64;
 
-    function setProperty(property, value) {
-      document.documentElement.style.setProperty(property, value);
-    }
-
-    function removeProperty(property) {
-      document.documentElement.style.removeProperty(property);
-    }
-
-    function updateHeaderStyles() {
-      const { top, height } = headerRef.current.getBoundingClientRect();
-      const scrollY = clamp(
-        window.scrollY,
-        0,
-        document.body.scrollHeight - window.innerHeight
-      );
-
-      if (isInitial.current) {
-        setProperty("--header-position", "sticky");
-      }
-
-      setProperty("--content-offset", `${downDelay}px`);
-
-      if (isInitial.current || scrollY < downDelay) {
-        setProperty("--header-height", `${downDelay + height}px`);
-        setProperty("--header-mb", `${-downDelay}px`);
-      } else if (top + height < -upDelay) {
-        const offset = Math.max(height, scrollY - upDelay);
-        setProperty("--header-height", `${offset}px`);
-        setProperty("--header-mb", `${height - offset}px`);
-      } else if (top === 0) {
-        setProperty("--header-height", `${scrollY + height}px`);
-        setProperty("--header-mb", `${-scrollY}px`);
-      }
-
-      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
-        setProperty("--header-inner-position", "fixed");
-        removeProperty("--header-top");
-        removeProperty("--avatar-top");
-      } else {
-        removeProperty("--header-inner-position");
-        setProperty("--header-top", "0px");
-        setProperty("--avatar-top", "0px");
-      }
-    }
-
-    function updateAvatarStyles() {
-      if (!isHomePage) {
-        return;
-      }
-
-      const fromScale = 1;
-      const toScale = 36 / 64;
-      const fromX = 0;
-      const toX = 2 / 16;
-
-      const scrollY = downDelay - window.scrollY;
-
-      let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale;
-      scale = clamp(scale, fromScale, toScale);
-
-      let x = (scrollY * (fromX - toX)) / downDelay + toX;
-      x = clamp(x, fromX, toX);
-
-      setProperty(
-        "--avatar-image-transform",
-        `translate3d(${x}rem, 0, 0) scale(${scale})`
-      );
-
-      const borderScale = 1 / (toScale / scale);
-      const borderX = (-toX + x) * borderScale;
-      const borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`;
-
-      setProperty("--avatar-border-transform", borderTransform);
-      setProperty("--avatar-border-opacity", scale === toScale ? 1 : 0);
-    }
-
     function updateStyles() {
-      updateHeaderStyles();
-      updateAvatarStyles();
+      updateHeaderStyles({ headerRef, isInitial, downDelay, upDelay });
+      updateAvatarStyles({ isHomePage, downDelay });
       isInitial.current = false;
     }
 
