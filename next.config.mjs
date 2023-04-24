e25b2104ac5be73d127871b70e1c9b15ae8a4586
Anton Ždanov <anton@azdanov.dev>
2023-04-24 15:13:39 +0300

    remove csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import rehypePrism from "@mapbox/rehype-prism";
-import nextSafe from "next-safe";
-
-const isDev = process.env.NODE_ENV !== "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,21 +9,6 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-  headers: () => [
-    {
-      source: "/:path*",
-      headers: nextSafe({
-        isDev,
-        contentSecurityPolicy: {
-          mergeDefaultDirectives: true,
-          "img-src": ["https: data: blob:"],
-          "script-src": [
-            "'sha256-COiD90rc+P2uaVVk9/ag5Fkb+hUKuRTCyRoZoyLTnJ0='",
-          ],
-        },
-      }),
-    },
-  ],
 };
 
 const withMDX = nextMDX({
