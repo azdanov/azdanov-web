a531e7fc68c4d719a86a4110443a08f0d2ff10e7
Anton Ždanov <anton@azdanov.dev>
2023-05-04 10:58:37 +0300

    update csp

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,10 @@ const ContentSecurityPolicy = `
   manifest-src 'self';
   base-uri 'none';
   form-action 'self';
-  script-src 'self' 'sha256-COiD90rc+P2uaVVk9/ag5Fkb+hUKuRTCyRoZoyLTnJ0=' 'sha256-zlqnbDt84zf1iSefLU/ImC54isoprH/MRiVZGskwexk='${
+  script-src 'self' 'sha256-COiD90rc+P2uaVVk9/ag5Fkb+hUKuRTCyRoZoyLTnJ0='${
     isDev ? " 'unsafe-eval'" : ""
   };
-  style-src 'self' 'unsafe-hashes'${isDev ? " 'unsafe-inline'" : ""};
+  style-src 'self' 'unsafe-hashes' 'unsafe-inline';
   font-src 'self';
   img-src 'self' data: https: blob:;
   frame-ancestors 'none';
