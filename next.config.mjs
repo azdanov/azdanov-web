66b07b6ff34f925ba4bcd9a73378499585d2d056
Anton Ždanov <anton@azdanov.dev>
2023-05-17 13:05:54 +0300

    update cicd

--- next.config.mjs
+++ next.config.mjs
@@ -48,6 +48,7 @@ const securityHeaders = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   pageExtensions: ["js", "jsx", "mdx"],
   reactStrictMode: true,
   experimental: {
