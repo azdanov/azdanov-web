1917ebd5f86bfa552949c3e1653b68cd50de7ffd
Anton Ždanov <anton@azdanov.dev>
2023-05-18 23:14:45 +0300

    fix footer button dark mode color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,15 +19,15 @@ export function Footer() {
       <Container.Outer>
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+            <div className="flex flex-col items-center justify-between gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200 sm:flex-row">
+              <div className="flex gap-6">
                 <NavLink href="/">Home</NavLink>
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
               </div>
               <button
                 onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-                className="text-sm transition hover:text-blue-500 dark:hover:text-blue-400"
+                className="transition hover:text-blue-500 dark:hover:text-blue-400"
               >
                 Back to top
               </button>
