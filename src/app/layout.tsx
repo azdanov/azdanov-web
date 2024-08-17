17f8da83f68bcec91497164c017f3d2df6dd9b1e
Anton Ždanov <anton@azdanov.dev>
2024-08-17 06:55:10 +0300

    feat: Update site metadata and viewport configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,21 @@ import "@/styles/tailwind.css";
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { type Metadata } from "next";
+import { type Metadata, Viewport } from "next";
 import { ReactNode } from "react";
 
 import { Providers } from "@/app/providers";
 import { Layout } from "@/components/Layout";
 
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#3777E1" },
+    { media: "(prefers-color-scheme: dark)", color: "#18181b" },
+  ],
+};
+
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
   title: {
     template: "%s - Anton Ždanov",
     default: "Anton Ždanov - Software Developer",
@@ -16,16 +24,17 @@ export const metadata: Metadata = {
   description:
     "Hey, I'm Anton, a software developer based in Tallinn. I work with front-end, back-end, database, and infrastructure technologies. In my free time, I like to play video games, enjoy doing fitness, read books and study to get better at what I do.",
   alternates: {
+    canonical: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
     types: {
-      "application/rss+xml": `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+      "application/rss+xml": "/feed.xml",
     },
   },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body className="flex h-full bg-white dark:bg-zinc-900 sm:bg-zinc-50 sm:dark:bg-black">
+    <html lang="en" className="antialiased" suppressHydrationWarning>
+      <body className="flex bg-white dark:bg-zinc-900 sm:bg-zinc-50 sm:dark:bg-black">
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
