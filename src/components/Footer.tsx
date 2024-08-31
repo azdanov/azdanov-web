4e3c372224e8d934b4bdbfec5268f9c63328afec
Anton Ždanov <anton@azdanov.dev>
2024-08-31 17:51:47 +0300

    chore: Update footer layout and make privacy policy link smaller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export function Footer() {
   return (
     <footer className="mt-20 flex-none md:mt-32">
       <ContainerOuter>
-        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+        <div className="border-t border-zinc-100 pb-10 pt-10 dark:border-zinc-700/40">
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200 sm:flex-row">
               <div className="flex gap-6">
@@ -34,15 +34,17 @@ export function Footer() {
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/privacy">Privacy</NavLink>
               </div>
               <button
                 type="button"
                 onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-                className="transition hover:text-blue-500 dark:hover:text-blue-400"
+                className="hidden transition hover:text-blue-500 dark:hover:text-blue-400 sm:block"
               >
                 Back to top
               </button>
+            </div>
+            <div className="mt-6 text-center text-xs text-zinc-500 dark:text-zinc-400 sm:text-left">
+              <NavLink href="/privacy">Privacy Policy</NavLink>
             </div>
           </ContainerInner>
         </div>
